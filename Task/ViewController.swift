804797b62624600ea61f77446d4a62c61b22b7d8
AsadZee <ibrahimasad231@gmail.com>
2024-04-17 21:15:29 +0500

    Task Complete

--- Task/ViewController.swift
+++ Task/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var followButton:UIButton!
+    @IBOutlet weak var like:UIView!
+    @IBOutlet weak var comment:UIView!
+    @IBOutlet weak var share:UIView!
+    @IBOutlet weak var bottomView:UIView!
+    @IBOutlet weak var descriptionView:UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        followButton.layer.cornerRadius = followButton.frame.size.width / 7
+        followButton.clipsToBounds = true
+        
+        like.layer.cornerRadius = like.frame.size.width / 7
+        like.clipsToBounds = true
+        
+        comment.layer.cornerRadius = comment.frame.size.width / 7
+        comment.clipsToBounds = true
+        
+        share.layer.cornerRadius = share.frame.size.width / 7
+        share.clipsToBounds = true
+        
+        descriptionView.layer.cornerRadius = descriptionView.frame.size.width / 7
+        descriptionView.clipsToBounds = true
+        
+//        let maskPath = UIBezierPath(roundedRect: bottomView.bounds,
+//                                             byRoundingCorners: [.topLeft, .topRight],
+//                                             cornerRadii: CGSize(width: 8, height: 8))
+//        let maskLayer = CAShapeLayer()
+//        maskLayer.frame = bottomView.bounds
+//        maskLayer.path = maskPath.cgPath
+//        bottomView.layer.mask = maskLayer
+
+       
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
